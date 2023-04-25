7eb629807b4ea5c1a4edc8082767427824999e5a
DominicRuano <Dojuparupe1975@gmail.com>
2023-04-25 12:05:17 -0600

    initial commit

---
+++ src/main/java/proyecto2/Proyecto2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package proyecto2;
+
+/**
+ *
+ * @author Dominic
+ */
+public class Proyecto2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
